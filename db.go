818fcf0e50d9046b3985bafd227eef6811cd7c2e
Vyacheslav Potoropin <slavapotoropin@gmail.com>
2024-05-11 23:18:40 +0200

    Enchanse text search by using sqlite FTS5

--- db.go
+++ db.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"strings"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -30,11 +31,31 @@ func NewDB() *LogitterDB {
 
 func (self *LogitterDB) Init() {
 	_, err := self.conn.Exec(`
+		-- Primary table
 		CREATE TABLE IF NOT EXISTS records (
 			id 					INTEGER NOT NULL PRIMARY KEY,
 			timestamp		INTEGER NOT NULL,
 			text				TEXT 		NOT NULL
 		);
+
+		-- Text index
+		CREATE VIRTUAL TABLE IF NOT EXISTS text_index USING FTS5(text);
+
+		-- Insert trigger
+		CREATE TRIGGER IF NOT EXISTS insert_text AFTER INSERT ON records BEGIN
+			INSERT INTO text_index(rowid, text) VALUES (new.rowid, new.text);
+		END;
+
+		-- Update trigger
+		CREATE TRIGGER IF NOT EXISTS update_text AFTER UPDATE ON records BEGIN
+			DELETE FROM text_index WHERE rowid = old.rowid;
+			INSERT INTO text_index(rowid, text) VALUES (new.rowid, new.text);
+		END;
+
+		-- Delete trigger
+		CREATE TRIGGER IF NOT EXISTS delete_text AFTER DELETE ON records BEGIN
+			DELETE FROM text_index WHERE rowid = old.rowid;
+		END;
 	`)
 	if err != nil {
 		panic(err) // TODO
@@ -73,10 +94,64 @@ func (self *LogitterDB) GetRecords() []Record {
 }
 
 func (self *LogitterDB) GetRecordsFilter(text string) []Record {
+	if len(text) == 0 {
+		return self.GetRecords()
+	}
 	cursor, err := self.conn.Query(
-		`SELECT id, timestamp, text FROM records WHERE text LIKE ?;`,
+		`SELECT rowid FROM text_index WHERE text MATCH ?;`,
+		fmt.Sprintf("%s OR %s*", text, text),
+	)
+	if err != nil {
+		panic(err) // TODO
+	}
+	defer cursor.Close()
+
+	var sb strings.Builder
+	sb.WriteString("SELECT * FROM records WHERE id in (")
+	ids_map := make(map[int]struct{})
+	for cursor.Next() {
+		var id int
+		err = cursor.Scan(&id)
+		if err != nil {
+			panic(err) // TODO
+		}
+		ids_map[id] = struct{}{}
+	}
+	cursor.Close()
+
+	cursor, err = self.conn.Query(
+		`SELECT id FROM records WHERE text LIKE ?;`,
 		fmt.Sprintf("%%%s%%", text),
 	)
+	if err != nil {
+		panic(err) // TODO
+	}
+	defer cursor.Close()
+	for cursor.Next() {
+		var id int
+		err = cursor.Scan(&id)
+		if err != nil {
+			panic(err) // TODO
+		}
+		ids_map[id] = struct{}{}
+	}
+	cursor.Close()
+
+	if len(ids_map) == 0 {
+		return nil
+	}
+
+	var ids []any
+	for id := range ids_map {
+		if len(ids) > 0 {
+			sb.WriteString(",")
+		}
+		sb.WriteString("?")
+		ids = append(ids, id)
+	}
+	sb.WriteString(");")
+
+	cursor, err = self.conn.Query(sb.String(), ids...)
 	if err != nil {
 		panic(err) // TODO
 	}
