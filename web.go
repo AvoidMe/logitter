ff18386db2db1f67f88015694f766ba3e3beb5bd
Vyacheslav Potoropin <slavapotoropin@gmail.com>
2024-05-19 19:48:02 +0200

    Parse urls in text entries, make it clickable

--- web.go
+++ web.go
@@ -3,7 +3,9 @@ package main
 import (
 	"embed"
 	"fmt"
+	"net/url"
 	"slices"
+	"strings"
 	"text/template"
 	"time"
 
@@ -24,11 +26,23 @@ type IndexPayload struct {
 	Records []Record
 }
 
-func Index(c echo.Context, db *LogitterDB) error {
-	// get data from db
-	records := db.GetRecords()
-	slices.Reverse(records)
+func ParseText(text string) string {
+	var sb strings.Builder
+	arr := strings.Split(text, " ")
+	for i, v := range arr {
+		if url, err := url.Parse(v); err == nil && (url.Scheme == "http" || url.Scheme == "https") {
+			templates.ExecuteTemplate(&sb, "URL", v)
+		} else {
+			sb.WriteString(v)
+		}
+		if len(arr) > 1 && i < len(arr)-1 {
+			sb.WriteString(" ")
+		}
+	}
+	return sb.String()
+}
 
+func IndexPayloadFromDBRecords(records []Record) []IndexPayload {
 	// make groups by day
 	payload := []IndexPayload{}
 	for _, v := range records {
@@ -37,8 +51,19 @@ func Index(c echo.Context, db *LogitterDB) error {
 		if len(payload) == 0 || payload[len(payload)-1].Day != currDay {
 			payload = append(payload, IndexPayload{Day: currDay})
 		}
+		v.Text = ParseText(v.Text)
 		payload[len(payload)-1].Records = append(payload[len(payload)-1].Records, v)
 	}
+	return payload
+}
+
+func Index(c echo.Context, db *LogitterDB) error {
+	// get data from db
+	records := db.GetRecords()
+	slices.Reverse(records)
+
+	// group by day
+	payload := IndexPayloadFromDBRecords(records)
 
 	// write response to the client
 	err := templates.ExecuteTemplate(c.Response().Writer, "Index", payload)
@@ -53,16 +78,8 @@ func Search(c echo.Context, db *LogitterDB) error {
 	records := db.GetRecordsFilter(c.QueryParam("query"))
 	slices.Reverse(records)
 
-	// make groups by day
-	payload := []IndexPayload{}
-	for _, v := range records {
-		currTime := time.Unix(v.Timestamp, 0)
-		currDay := fmt.Sprintf("%d-%02d-%02d", currTime.Year(), currTime.Month(), currTime.Day())
-		if len(payload) == 0 || payload[len(payload)-1].Day != currDay {
-			payload = append(payload, IndexPayload{Day: currDay})
-		}
-		payload[len(payload)-1].Records = append(payload[len(payload)-1].Records, v)
-	}
+	// group by day
+	payload := IndexPayloadFromDBRecords(records)
 
 	// write response to the client
 	err := templates.ExecuteTemplate(c.Response().Writer, "Items", payload)
